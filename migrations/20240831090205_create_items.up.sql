0783859891ac1b8b7056ac940a53ce698ea3fbd4
Svetlana Sotnikova <sotnikovas337@gmail.com>
2024-09-02 13:53:37 +0300

    upd

--- migrations/20240831090205_create_items.up.sql
+++ migrations/20240831090205_create_items.up.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
 CREATE TABLE items(
   id uuid DEFAULT public.gen_random_uuid() NOT NULL,
   name character varying NOT NULL
-)
+);
 CREATE TABLE refresh_tokens (
     user_id UUID PRIMARY KEY,
     token VARCHAR NOT NULL
